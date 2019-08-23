d781ae2d166222ac561a6967d804e5d2b86190a8
PretendHanjun <1406840449@qq.com>
2019-08-23 22:46:58 +0800

    -

---
+++ 基础练习/C++/杨辉三角.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	int a[34][34];
+	for(int i=0;i<n;i++)
+	{
+		a[i][0]=1;//第一列为1
+		a[i][i]=1;
+		for(int j=1;j<i;j++)
+			a[i][j]=a[i-1][j-1]+a[i-1][j];
+	}
+	for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<=i;j++)
+			cout<<a[i][j]<<" ";
+        cout<<endl;
+    }
+	return 0;
+}
